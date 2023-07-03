4700140510997832b603546b5956e4c58dd77aaf
Woochang Lee <changi112242@outlook.com>
2023-07-03 14:23:36 +0900

    Init project

---
+++ src/test/java/net/studio1122/boilerplate/SpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.studio1122.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
